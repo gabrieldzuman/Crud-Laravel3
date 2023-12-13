d91c4f47475ab42f6fbadab73ec2b378f71ada0e
Gabriel Dzuman <83187468+gabrieldzuman@users.noreply.github.com>
2023-12-13 11:21:37 -0300

    Add files via upload

---
+++ app/Models/Series.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Series extends Model
+{
+    use HasFactory;
+    protected $fillable = ['nome', 'cover'];
+
+    public function seasons()
+    {
+        return $this->hasMany(Season::class, 'series_id');
+    }
+
+    public function episodes()
+    {
+        return $this->hasManyThrough(Episode::class, Season::class);
+    }
+
+    protected static function booted()
+    {
+        self::addGlobalScope('ordered', function (Builder $queryBuilder) {
+            $queryBuilder->orderBy('nome');
+        });
+    }
+}
